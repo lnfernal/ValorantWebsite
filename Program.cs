037aacba59002cb685583928dea0b90dfe167733
Kye <promasterboy@outlook.com>
2022-04-19 11:10:40 +1000

    force localhost url

--- Program.cs
+++ Program.cs
@@ -4,25 +4,7 @@ using System.Diagnostics;
 using ValorantManager.Data;
 using ValorantManager.Services;
 
-
-#if !DEBUG
-if (!File.Exists("aspnetcorev2_inprocess.dll"))
-{
-    Console.Clear();
-    Console.WriteLine("Please extract Valorant Website.zip to a folder instead of doubling clicking it inside a zipping program.");
-
-    Console.ReadKey();
-    Process.GetCurrentProcess().Kill();
-}
-
-
 const string url = "http://localhost:4000";
-if (args.Length == 0)
-{
-    Process.Start(Process.GetCurrentProcess().ProcessName, "--urls " + url);
-    Process.GetCurrentProcess().Kill();
-    return;
-}
 
 ProcessStartInfo info = new ProcessStartInfo();
 info.FileName = url;
@@ -34,7 +16,6 @@ Process thisProc = Process.GetCurrentProcess();
 Process.GetProcessesByName(thisProc.ProcessName).Where(x => x.Id != thisProc.Id).All(x => { x.Kill(); return true; });
 
 
-#endif
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -51,10 +32,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    
 }
+app.UseHsts();
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
+//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
@@ -64,4 +47,4 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-app.Run();
+app.Run(url);
